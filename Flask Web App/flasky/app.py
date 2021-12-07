b9d51aeec4f603755ccbeb9b331da06c61e5d714
Svetozar Stojanovic <svetacar@gmail.com>
2021-12-07 15:02:47 +0100

    revamp templates and add json for bot data

--- Flask Web App/flasky/app.py
+++ Flask Web App/flasky/app.py
@@ -1,10 +1,33 @@
 from flask import Flask, render_template
 from flask import request
+import json 
 
 app = Flask(__name__)
 
+def check_if_bot_exists(name):
+    file = open('bot_data.json')
+    data = json.load(file) 
+
+    # bots list 
+    bots_list = data["bots"]
+    bots_list_names = []
+    for bot in bots_list:
+        bots_list_names.append(bot["bot_name"])
+
+    #print(bots_list_names)
+
+    if name in bots_list_names: 
+        return True
+    else: 
+        return False
+
+@app.errorhandler(404)
+def page_not_found(content_type):
+   return render_template('404.html', content_type=content_type)
+
 @app.route('/')
 def index():
+    '''Displays root URL.'''
 #    return '<h1>Hello World!</h1>'
 #    user_agent = request.headers.get('User-Agent')
 #    return '<p>Your browser is {}</p>'.format(user_agent)
@@ -12,22 +35,28 @@ def index():
 
 @app.route('/user/<name>')
 def template_user(name):
+    '''Displays user profile.'''
     return render_template('user.html', name=name)
 
-
-#@app.route('/user/<user_name>')
-#def user(user_name):
-#    return '<h1>Hello, {}!</h1>'.format(user_name)
-
-@app.route('/bot/train/<bot_name>')
+@app.route('/train/<bot_name>')
 def train_bot(bot_name):
     '''Sends request to a specific bot to train.'''
-    return '<h1>Training... {}!</h1>'.format(bot_name)
+    if not check_if_bot_exists(bot_name):
+        return page_not_found(bot_name)
+    ##return '<h1>Training... {}!</h1>'.format(bot_name)
+    return render_template('train.html', bot_name=bot_name)
 
-@app.route('/bot/logs/<bot_name>')
+@app.route('/logs/<bot_name>')
 def show_logs(bot_name):
     '''Shows logs page for a specific bot.'''
-    return '<h1>Logs... {}!</h1>'.format(bot_name)
+    ##return '<h1>Logs... {}!</h1>'.format(bot_name)
+    return render_template('logs.html', bot_name=bot_name)
+
+@app.route('/conversations/<bot_name>')
+def show_conversations(bot_name):
+    '''Shows logs page for a specific bot.'''
+    ##return '<h1>Logs... {}!</h1>'.format(bot_name)
+    return render_template('conversations.html', bot_name=bot_name)
 
 if __name__ == '__main__':
     app.run()
