4869ef394b4e371ee4617c8afe671c73261c3252
Svetozar Stojanovic <svetacar@gmail.com>
2021-12-19 01:08:54 +0100

    RAYSA-1 fix documentation for functions

--- Flask Web App/flasky/app.py
+++ Flask Web App/flasky/app.py
@@ -27,7 +27,7 @@ def page_not_found(content_type):
 
 @app.route('/')
 def index():
-    '''Displays root URL.'''
+    '''Returns a template for the index page.'''
 #    return '<h1>Hello World!</h1>'
 #    user_agent = request.headers.get('User-Agent')
 #    return '<p>Your browser is {}</p>'.format(user_agent)
@@ -35,12 +35,12 @@ def index():
 
 @app.route('/user/<username>')
 def template_user(username):
-    '''Displays user profile.'''
+    '''Returns a template for the user profile page.'''
     return render_template('user.html', username=username)
 
 @app.route('/train/<bot_name>')
 def train_bot(bot_name):
-    '''Sends request to a specific bot to train.'''
+    '''Returns a template for training overview for a specific bot.'''
     if not check_if_bot_exists(bot_name):
         return page_not_found(bot_name)
     ##return '<h1>Training... {}!</h1>'.format(bot_name)
@@ -48,15 +48,15 @@ def train_bot(bot_name):
 
 @app.route('/logs/<bot_name>')
 def show_logs(bot_name):
-    '''Shows logs page for a specific bot.'''
+    '''Returns a template for logs overview for a specific bot.'''
     if not check_if_bot_exists(bot_name):
         return page_not_found(bot_name)
     ##return '<h1>Logs... {}!</h1>'.format(bot_name)
     return render_template('logs.html', bot_name=bot_name)
 
 @app.route('/conversations/<bot_name>')
-def show_conversations(bot_name):
-    '''Shows logs page for a specific bot.'''
+def show_all_conversations(bot_name):
+    '''Returns a template for conversations overview for a specific bot.'''
     if not check_if_bot_exists(bot_name):
         return page_not_found(bot_name)
     ##return '<h1>Logs... {}!</h1>'.format(bot_name)
