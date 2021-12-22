8832fc551478897c719c2fed11aa24bbf5b8bf6c
Svetozar Stojanovic <svetacar@gmail.com>
2021-12-22 22:16:05 +0100

    RAYSA-1 comment out __init__ of Base model

--- Flask Web App/app/models/user_models.py
+++ Flask Web App/app/models/user_models.py
@@ -11,8 +11,8 @@ db = SQLAlchemy()
 class BaseModel(db.Model):
     """Base data model for all objects"""
     __abstract__ = True
-    def __init__(self, *args):
-        super().__init__(*args)
+    # def __init__(self, *args):
+    #     super().__init__(*args)
     def __repr__(self):
         """Define a base way to print models"""
         return '%s(%s)' % (self.__class__.__name__, {
