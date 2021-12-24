f9a2e41baa3ed47aedaea80a3cc2b4e6bc0a5410
Svetozar Stojanovic <svetacar@gmail.com>
2021-12-24 17:49:35 +0100

    RAYSA-1 register site blueprint

--- Flask Web App/app/views/__init__.py
+++ Flask Web App/app/views/__init__.py
@@ -1,6 +1,8 @@
 from .main_views import main_blueprint
 from .bot_views import bot_blueprint
+from .site_views import site_blueprint
 
 def register_blueprints(app):
     app.register_blueprint(main_blueprint)
     app.register_blueprint(bot_blueprint)
+    app.register_blueprint(site_blueprint)
