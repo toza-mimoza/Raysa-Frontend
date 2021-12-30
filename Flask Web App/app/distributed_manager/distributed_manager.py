46e54199e0808aa560f07c6d5157a718065ac0c9
Svetozar Stojanovic <svetacar@gmail.com>
2021-12-30 22:41:08 +0100

    RAYSA-72 add empty distibuted manager package

---
+++ Flask Web App/app/distributed_manager/distributed_manager.py
@@ -0,0 +1,6 @@
+# manager for distributed system
+# sends commands to nodes for: 
+#   triggering training of bots 
+#   file extraction (logs)
+#   on the other end there is an HTTP RESTful API for offering files to download 
+#   SECURITY: implement token based authentication for RESTful service (API keys)
