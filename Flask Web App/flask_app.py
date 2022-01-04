80815b32a15f1646ff8234aa987104905a2217a4
Svetozar Stojanovic <svetacar@gmail.com>
2022-01-04 21:55:20 +0100

    RAYSA-77 remove unused handler functions in flask_app.py

--- Flask Web App/flask_app.py
+++ Flask Web App/flask_app.py
@@ -2,21 +2,5 @@ from app import create_app
 
 app, socketio = create_app()
 
-
-@socketio.on("connect")
-def handle_connection():
-    print("Connected a client...")
-
-
-@socketio.on("message")
-def handle_message(msg):
-    print(f"Client message: {msg}")
-
-
-@socketio.on("UserSendsMessage")
-def handle_user_message(msg):
-    print(f"User message: {msg}")
-
-
 if __name__ == "__main__":
     socketio.run(app)
