376011f7ff6ee2b75615f766c718585db4fdcf5c
Svetozar Stojanovic <svetacar@gmail.com>
2022-01-04 21:56:39 +0100

    RAYSA-79 consider custom logging but not use it for now

--- Flask Web App/app/__init__.py
+++ Flask Web App/app/__init__.py
@@ -18,6 +18,8 @@ from app.common.extensions import cache
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from flask_socketio import SocketIO
+from .distributed_manager.distributed_manager import init_handlers
+from .logging_config.logging_config import init_logging
 
 # Instantiate Flask extensions
 csrf_protect = CSRFProtect()
@@ -113,6 +115,8 @@ def create_app(extra_config_settings={}):
     socketio = SocketIO(
         app, cors_allowed_origins="http://127.0.0.1:5000"
     )  # , cors_allowed_origins="http://127.0.0.1:5000")
+    init_handlers(socketio)
+    # init_logging()
 
     admin = Admin(app, name="Raysa", template_mode="bootstrap3")
     admin.add_view(ModelView(User, db.session))
