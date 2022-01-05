cedadf1d040a18b10b3423f68a8fccd90a0fc9a7
Svetozar Stojanovic <svetacar@gmail.com>
2022-01-06 00:20:46 +0100

    RAYSA-74 fix infinite recursion

--- Flask Web App/app/models/db.py
+++ Flask Web App/app/models/db.py
@@ -2,10 +2,13 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
 
 import datetime
+
 db = SQLAlchemy()
+
 
 class BaseModel(db.Model, SerializerMixin):
     """Base data model for all objects"""
+
     __abstract__ = True
     # def __init__(self, *args):
     # #     super().__init__(*args)
@@ -14,10 +17,8 @@ class BaseModel(db.Model, SerializerMixin):
 
     def __repr__(self):
         """Define a base way to print models"""
-        return '%s(%s)' % (self.__class__.__name__, {
-            column: value
-            for column, value in self.to_dict().items()
-        })
+        return "<{0.__class__.__name__}: created_on={0.created_on!r}>".format(self)
+
     # def json(self):
     #     """
     #     Define a base way to jsonify models, dealing with datetime objects
